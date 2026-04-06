63b0b2e095ca77c7e5f154ac92ce6513924acc4e
bhataasim1 <bhataasim700@gmail.com>
2026-04-06 16:00:08 +0530

    chore: update build configuration, ignore patterns, and package metadata for library distribution

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  // dts: true,
+  dts: {
+    compilerOptions: {
+      ignoreDeprecations: '6.0',
+    },
+  },
   clean: true,
   sourcemap: true,
 });
