691432029675d0045f35150b9ae9d003adec7c94
bhataasim1 <bhataasim700@gmail.com>
2026-04-07 09:20:37 +0530

    feat: implement ProductsAPI module with create product functionality and type definitions

---
+++ src/modules/products/products.api.ts
@@ -0,0 +1,19 @@
+
+import { HttpClient } from '../../core/https';
+import {
+  CreateProductInput,
+  ProductResponse,
+} from './products.types';
+
+export class ProductsAPI {
+  constructor(private http: HttpClient) { }
+
+  create(
+    data: CreateProductInput
+  ): Promise<ProductResponse> {
+    return this.http.request('/v1/payment/product/create', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  }
+}
---
+++ src/modules/products/products.types.ts
@@ -0,0 +1,23 @@
+import { StripeMetaData } from "../../types/types";
+
+export type CreateProductInput = {
+  name: string;
+  description?: string;
+  images?: string[];
+  metadata?: StripeMetaData;
+};
+
+export type ProductResponse = {
+  id: string;
+  name: string;
+  description?: string | null;
+  active: boolean;
+  created: number;
+  deleted?: void;
+  images: Array<string>;
+  livemode: boolean;
+  metadata: StripeMetaData;
+  unit_label?: string | null;
+  updated: number;
+  url: string | null;
+};
